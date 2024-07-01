7cc1e773a7050c2c7864ae3443b93d5b63f655f2
eyad-hussein <eyad.hussein@ejust.edu.eg>
2024-07-01 11:42:03 +0300

    feat: implement echo utility

---
+++ src/Coreutils-EYAD_HUSSEIN-python/echo.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+def echo() -> None:
+    parser = argparse.ArgumentParser(
+        "echo", description="Prints the given strings to the standard output"
+    )
+    parser.add_argument("strings", nargs="+", help="The strings to be printed")
+    parser.add_argument(
+        "-n", action="store_true", help="Do not print the trailing newline character"
+    )
+
+    args = parser.parse_args()
+
+    output = " ".join(args.strings)
+    if args.n:
+        print(output, end="")
+    else:
+        print(output)
+
+
+if __name__ == "__main__":
+    echo()
